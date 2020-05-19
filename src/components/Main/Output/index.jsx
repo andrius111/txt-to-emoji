a130c355c1b7bb049b629508f7c73a0784bb45fe
Andrius Martini <andrius.martini1994@gmail.com>
2020-05-18 23:45:51 -0300

    Initialized the download option

--- src/components/Main/Output/index.jsx
+++ src/components/Main/Output/index.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Parser from 'html-react-parser'
+import { AiOutlineCloudDownload } from 'react-icons/ai'
+import html2canvas from 'html2canvas'
 
 import Style from './style.module.scss'
 
 const Output = (props) => {
+  const handleDownload = () => {
+    html2canvas(document.querySelector('[data-testid=output-div]')).then(canvas => {
+      const link = document.createElement('a')
+      link.download = 'txt-to-emoji'
+      link.href = canvas.toDataURL('image/png')
+      link.click()
+    })
+  }
+
   return (
     <div 
       className={ Style.output_container }
@@ -17,6 +28,16 @@ const Output = (props) => {
         data-testid="output-div"
         style={ props.style }
       >
+        <div className={ Style.output_overlay }>
+          <AiOutlineCloudDownload 
+            color="white"
+            cursor="pointer"
+            onClick={ () => handleDownload() }
+            size={ 60 }
+            title="Download"
+          />
+        </div>
+
         { Parser(props.value) }
       </div>
     </div>
